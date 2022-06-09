f67eecc071e019ca0ce8e8861ac314d801e5489b
Antonio Carmona <antoniocb978@gmail.com>
2022-06-09 08:59:33 +0200

    refactor: extraer la inferfaz "Vehicle"

---
+++ src/com/antonio/examen/junio/Vehicle.java
@@ -0,0 +1,13 @@
+package com.antonio.examen.junio;
+
+interface Vehicle {
+
+	void changeGear(int newGear);
+
+	void speedUp(int increment);
+
+	void applyBrakes(int decrement);
+
+	void printStates();
+
+}
---
+++ src/com/antonio/examen/junio/package-info.java
@@ -0,0 +1,1 @@
+package com.antonio.examen.junio;
