5b78dd1b400e1146ce204b6538042f206b58b803
Antonio Carmona <antoniocb978@gmail.com>
2022-06-09 08:43:59 +0200

    "refactor: encapsula atributos"

--- src/com/examen/junio/Bicycle.java
+++ src/com/examen/junio/Bicycle.java
@@ -2,31 +2,51 @@ package com.examen.junio;
 
 
 class Bicycle {  
-  int speed;
-  int gear;
+  private int speed;
+  private int gear;
     
   public void changeGear(int newGear){     
-      gear = newGear;
+      setGear(newGear);
   }
     
   
   public void speedUp(int increment){
-      speed = speed + increment;
+      setSpeed(getSpeed() + increment);
   }
     
   public void applyBrakes(int decrement){     
-      speed = speed - decrement;
+      setSpeed(getSpeed() - decrement);
   }
     
   public void printStates() {
-      System.out.println("speed: " + speed
-          + " gear: " + gear);
+      System.out.println("speed: " + getSpeed()
+          + " gear: " + getGear());
   }
 
 
 @Override
 public String toString() {
-	return "Bicycle [speed=" + speed + ", gear=" + gear + "]";
+	return "Bicycle [speed=" + getSpeed() + ", gear=" + getGear() + "]";
+}
+
+
+int getSpeed() {
+	return speed;
+}
+
+
+void setSpeed(int speed) {
+	this.speed = speed;
+}
+
+
+int getGear() {
+	return gear;
+}
+
+
+void setGear(int gear) {
+	this.gear = gear;
 }
   
 }
